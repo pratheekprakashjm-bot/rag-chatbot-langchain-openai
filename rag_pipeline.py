639a5fb264b0d372644012d77634e599b7b85a50
kannan625-sigilotech <kannan625@sigilotech.com>
2026-06-30 20:24:25 +0530

    final commit

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -2,10 +2,9 @@ import os
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
-from langchain_openai import ChatOpenAI
 from langchain.chains import RetrievalQA
 
 load_dotenv()
